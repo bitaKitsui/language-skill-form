5c9f42e774cfccaa516af807e888303ec298e1a4
bitakitsui <takaki_kitsui@bita.jp>
2022-02-02 17:53:10 +0900

    feat: buttonコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import BaseInput from './components/atoms/BaseInput.vue'
 import BaseTitle from './components/atoms/BaseTitle.vue'
 import BaseSelect from './components/atoms/BaseSelect.vue'
 import { LANGUAGE_LEVEL_SELECTS } from './constants'
+import BaseButton from './components/atoms/BaseButton.vue'
 </script>
 
 <template>
@@ -15,6 +16,7 @@ import { LANGUAGE_LEVEL_SELECTS } from './constants'
     :name="'level-select'"
     :options="LANGUAGE_LEVEL_SELECTS"
   />
+  <BaseButton :type="'button'" :label="'送信'" :color="'primary'" />
 </template>
 
 <style></style>
---
+++ src/components/atoms/BaseButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+
+interface Props {
+  label: string
+  type: HTMLButtonElement
+  color: 'primary' | 'error' | 'disabled'
+}
+
+const props = defineProps<Props>()
+const { label, type, color } = toRefs(props)
+</script>
+
+<template>
+  <button :type="type" :color="color">
+    {{ label }}
+  </button>
+</template>
