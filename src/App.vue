7a413cdd55bce7534b06aba0cffd0ffd1c2b1ea0
bitakitsui <takaki_kitsui@bita.jp>
2022-02-02 17:34:10 +0900

    feat: titleコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import BaseInput from './components/atoms/BaseInput.vue'
+import BaseTitle from './components/atoms/BaseTitle.vue'
 </script>
 
 <template>
   <BaseInput :id="'toeic'" :label="'Toeic'" :type="'text'" />
   <BaseInput :id="'toefl'" :label="'Toefl'" :type="'text'" />
+  <BaseTitle :value="'語学力(英語)'" />
 </template>
 
 <style></style>
---
+++ src/components/atoms/BaseTitle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+
+interface Props {
+  value: string
+}
+
+const props = defineProps<Props>()
+const { value } = toRefs(props)
+</script>
+
+<template>
+  <p class="title">
+    {{ value }}
+  </p>
+</template>
+
+<style scoped>
+.title {
+  color: #818181;
+}
+</style>
