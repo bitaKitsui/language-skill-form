871dd5faf608aeacffaf95ab49bad3406dc06743
bitakitsui <takaki_kitsui@bita.jp>
2022-02-03 15:52:03 +0900

    feat: 英語フォームコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,9 @@
 <script setup lang="ts">
-import BaseInput from './components/atoms/BaseInput.vue'
-import BaseTitle from './components/atoms/BaseTitle.vue'
-import BaseSelect from './components/atoms/BaseSelect.vue'
-import { LANGUAGE_LEVEL_SELECTS } from './constants'
-import BaseButton from './components/atoms/BaseButton.vue'
-import BaseIconButton from './components/atoms/BaseIconButton.vue'
+import BaseForm from './components/molecules/BaseForm.vue'
 </script>
 
 <template>
-  <BaseInput :id="'toeic'" :label="'Toeic'" :type="'text'" />
-  <BaseInput :id="'toefl'" :label="'Toefl'" :type="'text'" />
-  <BaseTitle :value="'語学力(英語)'" />
-  <BaseSelect
-    :id="'level'"
-    :label="'レベル'"
-    :name="'level-select'"
-    :options="LANGUAGE_LEVEL_SELECTS"
-  />
-  <BaseButton :type="'button'" :label="'送信'" :color="'primary'" />
-  <BaseIconButton
-    :type="'button'"
-    :label="'言語を追加する'"
-    :color="'primary'"
-    :outlined="true"
-  />
+  <BaseForm />
 </template>
 
 <style></style>
---
+++ src/components/molecules/BaseForm.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import BaseTitle from '../atoms/BaseTitle.vue'
+import BaseInput from '../atoms/BaseInput.vue'
+import BaseSelect from '../atoms/BaseSelect.vue'
+import { LANGUAGE_LEVEL_SELECTS } from '../../constants'
+</script>
+
+<template>
+  <div class="wrapper">
+    <BaseTitle :value="'語学力（英語）'" />
+    <form class="form">
+      <BaseSelect
+        :id="'level'"
+        :label="'レベル'"
+        :name="'select-level'"
+        :options="LANGUAGE_LEVEL_SELECTS"
+      />
+      <BaseInput :id="'toeic'" :label="'TOEIC'" :type="'text'" />
+      <span>点</span>
+      <BaseInput :id="'toefl'" :label="'TOEFL'" :type="'text'" />
+      <span>点</span>
+    </form>
+  </div>
+</template>
+
+<style>
+.wrapper {
+  display: flex;
+}
+
+.form {
+  display: flex;
+}
+</style>
