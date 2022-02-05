fe94b75c65c5f6d1f91b8a77ce0375af9ebaa54b
bitakitsui <takaki_kitsui@bita.jp>
2022-02-06 08:16:15 +0900

    feat: formlist追加

---
+++ src/components/molecules/BaseFormList.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import BaseIconButton from '../atoms/BaseIconButton.vue'
+import BaseSelects from '../molecules/BaseSelects.vue'
+</script>
+
+<template>
+  <ul class="form-list">
+    <li class="form-border">
+      <div class="form-header">
+        <BaseIconButton
+          :label="'削除'"
+          :icon="'×'"
+          :type="'button'"
+          color="'primary'"
+          :outlined="false"
+          :width="'80px'"
+        />
+      </div>
+      <BaseSelects :height="'170px'" />
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.form-list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.form-border {
+  border: 1px solid #3d60d5;
+  margin-bottom: 20px;
+}
+
+.form-header {
+  width: 100%;
+  height: 40px;
+  background: #f2f3f7;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/components/organisms/BaseOtherLanguageForm.vue
+++ src/components/organisms/BaseOtherLanguageForm.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import BaseTitle from '../atoms/BaseTitle.vue'
-import BaseIconButton from '../atoms/BaseIconButton.vue'
 import BaseButtons from '../molecules/BaseButtons.vue'
-import BaseSelects from '../molecules/BaseSelects.vue'
+import BaseFormList from '../molecules/BaseFormList.vue'
 
 interface Props {
   width: string
@@ -17,19 +16,7 @@ const { width } = toRefs(props)
   <div class="other-language-wrapper">
     <BaseTitle :value="'語学力（その他言語）'" :width="'300px'" />
     <form class="other-language-form">
-      <div class="form-border">
-        <div class="form-header">
-          <BaseIconButton
-            :label="'削除'"
-            :icon="'×'"
-            :type="'button'"
-            color="'primary'"
-            :outlined="false"
-            :width="'80px'"
-          />
-        </div>
-        <BaseSelects :height="'170px'" />
-      </div>
+      <BaseFormList />
       <BaseButtons />
     </form>
   </div>
@@ -43,18 +30,5 @@ const { width } = toRefs(props)
 
 .other-language-form {
   width: v-bind(width);
-}
-
-.form-border {
-  border: 1px solid #3d60d5;
-  margin-bottom: 20px;
-}
-
-.form-header {
-  width: 100%;
-  height: 40px;
-  background: #f2f3f7;
-  display: flex;
-  justify-content: flex-end;
 }
 </style>
